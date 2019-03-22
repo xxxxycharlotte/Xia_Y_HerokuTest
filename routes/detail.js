de9981164452a45ef82e819ddfad5d82b1296141
xxxxycharlotte <550356048xyc@gmail.com>
2019-03-21 21:59:04 -0400

    add deatail.js

--- routes/detail.js
+++ routes/detail.js
@@ -6,11 +6,13 @@ const allData = require('../data');
 router.get('/:product', function(req, res) {
   let data = allData[req.params.product];
   if (data) {
+  	data['title'] = 'Converse | Product: ' + data.name;
     console.log('product:', data);
     res.render('detail', data);
     return;
   }
   res.render('404', {
+  	title:'Converse | Page not found',
     message: "Sorry, the page you're looking for is not found!"
   });
 });
